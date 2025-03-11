8dc4d5050fecb525436917afeb8630fc2d6a9d9b
joaommj <joao_jacinto_8@hotmail.com>
2025-03-11 22:13:49 +0000

    day1

--- React-Apartment-Proyect/src/App.jsx
+++ React-Apartment-Proyect/src/App.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
-
-
+/*import { useState } from 'react'*/
 import './App.css'
-
+import logo from "./assets/zylo-logo1.png"
 function App() {
   return (
-    <div className="App">
-      
-      <div className='navbar'>
-      
-      </div> 
-  
+    <div className="app">    
+    <div className='navbar'>   
+      <img id ="logo" src={logo}/>
+      <nav>
+        <div className='sidebar'> 
+          <a href="#">Home</a>
+          <a href="#">About Us</a>
+        </div>
+      </nav>
+    </div>  
+    <div className="main-container">
 
-
-
-
-      <footer> <a href=" https://github.com/Leinyem/Proyect-React---Apartments"> OurProyect</a> 
+    </div>
+      <footer> <a href=" https://github.com/Leinyem/Proyect-React---Apartments"> Zylo - Apartament Management </a> 
       </footer>
     </div>
   )
