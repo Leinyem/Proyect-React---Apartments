2ea55a742a6e37b2eb4fd7e6c0aac185e5fbb475
John Doe <johndoe@example.com>
2025-03-11 18:01:14 +0100

    Apartment proyect Day 1

---
+++ React-Apartment-Proyect/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className='navbar'>
+      
+      </div> 
+  
+
+
+
+
+      <footer> <a href=" https://github.com/Leinyem/Proyect-React---Apartments"> OurProyect</a> 
+      </footer>
+    </div>
+  )
+}
+
+export default App
