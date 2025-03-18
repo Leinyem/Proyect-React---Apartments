85e9542073e84d3638ea115fb28720806a5e0c2b
John Doe <johndoe@example.com>
2025-03-19 00:23:18 +0100

    all ok

--- React-Apartment-Proyect/src/components/ApartmentList.jsx
+++ React-Apartment-Proyect/src/components/ApartmentList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ApartmentCard from './ApartmentCard';
 
 export const ApartmentList = ({apartmentsArray, setApartmentsArray}) => {
